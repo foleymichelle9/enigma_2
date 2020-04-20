d1e6fff7f1014a2f3d9fe2243bf2af9695d36520
michelle <foleymichelle9@gmail.com>
2020-04-20 13:57:59 -0600

    Write enigma test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,8 +1,13 @@
 require_relative 'test_helper'
+require './lib/enigma'
 require './lib/shift'
-require "./lib/enigma"
 require 'date'
 
 class EnigmaTest < Minitest::Test
-  
+
+  def test_it_exists
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+  end
 end
