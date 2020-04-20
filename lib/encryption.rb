bd5b3fa27f2c0b0a327a6a1ac91e244fbf34bb91
michelle <foleymichelle9@gmail.com>
2020-04-20 11:23:16 -0600

    Set up encrypt

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,2 +1,10 @@
-class Encryption
-end 
+require_relative './enigma'
+require_relative './shift'
+require 'date'
+
+
+enigma = Enigma.new
+shift = Shift.new
+handle = File.open(ARGV[0], "r")
+message_text = handle.read
+handle.close
