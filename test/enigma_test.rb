125b14ad5e1552a92e5ed23ca6fecbcfe0d87557
michelle <foleymichelle9@gmail.com>
2020-04-20 14:12:32 -0600

    work on enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,4 +10,13 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_encrypt
+    enigma = Enigma.new
+    expected = { encryption: 'keder ohulw',
+                 key: '12345',
+                 date: '190420' }
+
+    assert_equal expected, enigma.encrypt('hello world', '012345', '190420')
+  end
 end
