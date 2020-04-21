815748a846e04f01674aa7f5bff022df50abfcb5
michelle <foleymichelle9@gmail.com>
2020-04-20 18:13:41 -0600

    Refactor enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,28 +5,27 @@ require 'date'
 
 class EnigmaTest < Minitest::Test
 
-  def test_it_exists
-    enigma = Enigma.new
+  def setup
+    @enigma = Enigma.new
+  end
 
-    assert_instance_of Enigma, enigma
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
   end
 
   def test_encrypt
-    enigma = Enigma.new
     expected = { encryption: 'zescf cfilk',
                  key: '12345',
                  date: '190420' }
 
-    assert_equal expected, enigma.encrypt('hello world', '12345', '190420')
+    assert_equal expected, @enigma.encrypt('hello world', '12345', '190420')
   end
 
   def test_decrypt
-   enigma = Enigma.new
-
    expected = { decryption: 'hello world',
                 key: '12345',
                 date: '190420' }
 
-   assert_equal expected, enigma.decrypt('zescf cfilk', '12345', '190420')
+   assert_equal expected, @enigma.decrypt('zescf cfilk', '12345', '190420')
   end
 end
